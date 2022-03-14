93a06f09c6be3ecf9a8acd77509307e19ae7e14d
Ezo Işıl Özçelik <e.ezoozcelik@gmail.com>
2022-03-15 02:15:10 +0300

    Raycast added

--- Trisolaris/Assets/Scripts/Mover.cs
+++ Trisolaris/Assets/Scripts/Mover.cs
@@ -6,10 +6,15 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
-   
+
+    Ray lastRay;
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100,Color.blue);
         gameObject.GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
