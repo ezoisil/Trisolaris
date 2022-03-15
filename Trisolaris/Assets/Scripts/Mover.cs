771e36be524f13fcaa1ef9165729663146ce738f
Ezo Işıl Özçelik <e.ezoozcelik@gmail.com>
2022-03-15 20:24:37 +0300

    Animation is connected to moving script

--- Trisolaris/Assets/Scripts/Mover.cs
+++ Trisolaris/Assets/Scripts/Mover.cs
@@ -6,13 +6,29 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
+    NavMeshAgent navMeshAgent;
+    Animator animator;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             MoveToCursor();
         }
+        UpdateAnimator();
+    }
 
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float forwardSpeed = localVelocity.z;
+        animator.SetFloat("forwardSpeed", forwardSpeed);
     }
 
     private void MoveToCursor()
@@ -22,7 +38,7 @@ public class Mover : MonoBehaviour
         bool hasHit = Physics.Raycast(ray, out hit);
         if (hasHit)
         {
-            gameObject.GetComponent<NavMeshAgent>().destination = hit.point;
+            navMeshAgent.destination = hit.point;
         }
     }
 }
