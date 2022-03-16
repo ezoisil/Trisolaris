e54f2fddd58606d87ebaaedc9099d529d5816643
Ezo Işıl Özçelik <e.ezoozcelik@gmail.com>
2022-03-17 00:34:58 +0300

    Addad combat system base

---
+++ Trisolaris/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Trisolaris.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attack(CombatTarget target)
+        {
+
+        }
+    }
+}
