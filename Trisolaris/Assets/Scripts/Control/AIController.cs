71ae80c7657658fea639e2e8bac73bd6655f352b
Ezo Işıl Özçelik <e.ezoozcelik@gmail.com>
2022-03-25 11:50:19 +0300

    Suspicion behaviour added

--- Trisolaris/Assets/Scripts/Control/AIController.cs
+++ Trisolaris/Assets/Scripts/Control/AIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Trisolaris.Combat;
 using Trisolaris.Core;
+using Trisolaris.Movement;
 using UnityEngine;
 
 namespace Trisolaris.Control
@@ -9,18 +10,25 @@ namespace Trisolaris.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 4f;
 
         Fighter fighter;
         GameObject player;
         Health health;
+        Mover mover;
+        ActionScheduler actionScheduler;
 
         Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
+        
 
         private void Awake()
         {
             fighter = GetComponent<Fighter>();
             player = GameObject.FindWithTag("Player");
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+
         }
         private void Start()
         {
@@ -29,17 +37,39 @@ namespace Trisolaris.Control
 
         private void Update()
         {
+            
             if (health.IsDead()) return;
 
             if (InAttackRangeOfPlayer()  && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                AttackBehaviour();
+                timeSinceLastSawPlayer = 0;
+            }
+            else if (timeSinceLastSawPlayer<suspicionTime)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
             }
-            
+
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         private bool InAttackRangeOfPlayer()
