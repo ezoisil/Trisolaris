97218ace4dca6e3bb5c5bb63de7dc3c4d4056a56
Ezo Işıl Özçelik <e.ezoozcelik@gmail.com>
2022-04-01 19:33:33 +0300

    Added player spawn point

--- Trisolaris/Assets/Scripts/Scene Management/Portal.cs
+++ Trisolaris/Assets/Scripts/Scene Management/Portal.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace Trisolaris.SceneManagement
@@ -8,6 +10,7 @@ namespace Trisolaris.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
         private void OnTriggerEnter(Collider other)
         {
             if(other.tag == "Player")
@@ -20,8 +23,31 @@ namespace Trisolaris.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("portal");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+
             Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+
         }
     }
 }
